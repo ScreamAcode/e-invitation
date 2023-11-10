6aa56f4c6085df948d2a82abfec3778e0208d232
ScreamAcode <ceo@screamacode.com>
2023-11-10 20:15:57 +0530

    intial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,58 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [showVideo, setShowVideo] = useState(false);
+  const [hideVideo, setHideVideo] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowVideo(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (showVideo) {
+      const timer = setTimeout(() => {
+        setHideVideo(true);
+      }, 11000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showVideo]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.weddingInvitation}>
+      {!hideVideo && (
+        <div
+          className={`${styles.imageCtn} ${showVideo ? styles.slideUp : ""}`}
+        >
+          <img className={styles.image} src="image.png" alt="Groom and Bride" />
         </div>
+      )}
+      {showVideo && (
+        <div
+          className={`${styles.videoCtn} ${hideVideo ? styles.slideUp : ""}`}
+        >
+          <video className={styles.video} playsInline autoPlay muted>
+            <source src="video.mp4" type="video/mp4" />
+          </video>
+        </div>
+      )}
+      <div className={`${styles.btnCtn} ${hideVideo ? styles.showBtn : ""}`}>
+        <button
+          className={styles.locationButton}
+          onClick={() => {
+            window.location.href = "https://maps.app.goo.gl/yLS5yy8ndYzdT7R1A";
+          }}
+        >
+          Get Location
+        </button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
