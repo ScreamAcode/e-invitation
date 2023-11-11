cfc9d7020f1e10485e0eec513843ccc2c2118fcc
ScreamAcode <ceo@screamacode.com>
2023-11-11 12:30:51 +0530

    new changes

--- app/page.js
+++ app/page.js
@@ -5,54 +5,39 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const [showVideo, setShowVideo] = useState(false);
-  const [hideVideo, setHideVideo] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowVideo(true);
-    }, 5000);
+    }, 3500);
 
     return () => clearTimeout(timer);
   }, []);
 
-  useEffect(() => {
-    if (showVideo) {
-      const timer = setTimeout(() => {
-        setHideVideo(true);
-      }, 11000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [showVideo]);
-
   return (
     <div className={styles.weddingInvitation}>
-      {!hideVideo && (
-        <div
-          className={`${styles.imageCtn} ${showVideo ? styles.slideUp : ""}`}
-        >
-          <img className={styles.image} src="image.png" alt="Groom and Bride" />
-        </div>
-      )}
+      <div className={`${styles.imageCtn} ${showVideo ? styles.slideUp : ""}`}>
+        <img className={styles.image} src="image.png" alt="Groom and Bride" />
+      </div>
       {showVideo && (
-        <div
-          className={`${styles.videoCtn} ${hideVideo ? styles.slideUp : ""}`}
-        >
-          <video className={styles.video} playsInline autoPlay muted>
+        <div className={styles.main}>
+          <div className={styles.content}>
+            <h2>Wedding Invitation</h2>
+            <button
+              className={styles.btn}
+              onClick={() => {
+                window.location.href =
+                  "https://maps.app.goo.gl/yLS5yy8ndYzdT7R1A";
+              }}
+            >
+              Tap for location
+            </button>
+          </div>
+          <video className={styles.video} controls>
             <source src="video.mp4" type="video/mp4" />
           </video>
         </div>
       )}
-      <div className={`${styles.btnCtn} ${hideVideo ? styles.showBtn : ""}`}>
-        <button
-          className={styles.locationButton}
-          onClick={() => {
-            window.location.href = "https://maps.app.goo.gl/yLS5yy8ndYzdT7R1A";
-          }}
-        >
-          Get Location
-        </button>
-      </div>
     </div>
   );
 }
